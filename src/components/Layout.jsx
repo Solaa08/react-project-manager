b2015ffec32b3a88b756239804edc57b0b9142e8
Solaa08 <solarium_08@hotmail.fr>
2022-09-21 17:05:22 +0200

    transformed my datas array in component into props from parent

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,11 +4,20 @@ import List from './List'
 import Card from './Card'
 
 export default function Layout() {
+
+  const projects = [
+    { name: 'React Movies', initials: 'RM', href: '#', createdAt: '12/12/2022', link: 'https://github.com/Solaa08/react-movie' ,bgColor: 'bg-blue-600' },
+    { name: 'React Restaurant', initials: 'RR', href: '#', createdAt: '12/12/2022', link: 'https://github.com/Solaa08/react-onepage' ,bgColor: 'bg-blue-600' },
+    { name: 'React Todolist', initials: 'RT', href: '#', createdAt: '12/12/2022', link: 'https://github.com/Solaa08/react-todolist' ,bgColor: 'bg-blue-600' },
+    { name: 'Native Todolist', initials: 'NT', href: '#', createdAt: '12/12/2022', link: 'https://github.com/Solaa08/todolistJS' ,bgColor: 'bg-yellow-500' },
+    { name: 'Native Video Game', initials: 'NVG', href: '#', createdAt: '12/12/2022', link: 'https://github.com/Solaa08/todolistJS' ,bgColor: 'bg-red-500' },
+]
+
   return (
     <body>
     <Navbar/>
     <Card/>
-    <List/>
+    <List props={projects}/>
     </body>
 
   )
